0d2d44deea84e44e99c14e389a148ff361cfc396
zongbao.zzb <zongbao.zzb@alibaba-inc.com>
2017-01-31 15:47:10 +0800

    init commit

---
+++ mylab-test/src/main/java/com/ailease/test/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.ailease.test;
+
+/**
+ * Created by zongbao.zzb on 2017/1/31.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
